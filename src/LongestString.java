7b7f4b02d41dd7b224b8fca20c1d669f67faafcb
sthiyag <sivaprakash_thiyagarajan@trimble.com>
2016-11-28 10:23:11 +0530

    Copy code

---
+++ src/LongestString.java
@@ -0,0 +1,17 @@
+/**
+ * Created by sthiyag on 11/25/2016.
+ */
+public class LongestString {
+
+    public int longestSubstring(String s, int k) {
+        int longestCount = 0;
+        return longestCount;
+    }
+
+    public static void main(String a[]){
+        LongestString longestString = new LongestString();
+        //longestString.longestSubstring("",);
+    }
+
+
+}
